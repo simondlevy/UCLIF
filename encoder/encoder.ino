3ac0b4c90179cd4913858012463a0e81889c192e
Simon D. Levy <simon.d.levy@gmail.com>
2026-04-21 15:42:36 -0400

    modified:   encoder.ino

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,33 +1,22 @@
 static const uint8_t PWM_PIN = 9;  // PWM output pin
 
-static const uint8_t POT_PIN = A0; // Potentiometer for frequency control
-
 static const uint32_t SPIKE_DURATION_USEC = 500;
 
-static void sendSpike()
-{
-    digitalWrite(PWM_PIN, HIGH);
-    delayMicroseconds(SPIKE_DURATION_USEC);
-    digitalWrite(PWM_PIN, LOW);
-}
+const float FREQ_HZ = 40;
 
 void setup()
 {
-    Serial.begin(115200);
-
     pinMode(PWM_PIN, OUTPUT);
 }
 
 void loop()
 { 
-    //const auto potValue = analogRead(POT_PIN);
+    // Delay for inter-spike interval
+    delayMicroseconds(1'000'000 / FREQ_HZ);
 
-    //const int delayTime = map(potValue, 0, 1023, 1000, 50);
-
-    //const float FREQ_HZ = 2000;
-
-    delayMicroseconds(100'000);
-
-    sendSpike();
+    // Send spike
+    digitalWrite(PWM_PIN, HIGH);
+    delayMicroseconds(SPIKE_DURATION_USEC);
+    digitalWrite(PWM_PIN, LOW);
 
 }
