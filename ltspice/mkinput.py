d07991a2422df8ae2d7fdb9ad3450bfc591e1a1e
Simon D. Levy <simon.d.levy@gmail.com>
2026-04-23 01:25:20 -0400

    new file:   mkinput.py

---
+++ ltspice/mkinput.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.arange(15000)
+
+t100 = t % 200
+
+v = t // 2500 + 2
+
+v[t100>9] = 0
+
+t = t.astype(float) / 1000
+
+plt.plot(t, v)
+plt.show()
