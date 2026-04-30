fff81acb8dadc2e39b032057750e3539620a7038
Simon D. Levy <simon.d.levy@gmail.com>
2026-04-30 01:16:59 -0400

    modified:   mkinput2.py

--- ltspice/mkinput2.py
+++ ltspice/mkinput2.py
@@ -8,6 +8,7 @@ NSAMP = 5000
 FREQ_START_HZ = 2
 FREQ_END_HZ= 10
 SPIKE_VOLTS = 3.3
+SPIKE_DUR_USEC = 50
 
 t = np.arange(NSAMP)
 
@@ -20,5 +21,7 @@ p = signal.find_peaks(s)[0]
 v = np.zeros(NSAMP)
 v[p] = 3.3
 
+plt.figure(figsize=(20, 6))
 plt.plot(t, v)
+plt.ylabel('Volts')
 plt.show()
