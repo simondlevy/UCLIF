d8c5745ce697d30ad0c5fe01ea90ad16542ebbe5
Simon D. Levy <simon.d.levy@gmail.com>
2026-04-30 01:10:48 -0400

    modified:   mkinput2.py

--- ltspice/mkinput2.py
+++ ltspice/mkinput2.py
@@ -2,14 +2,23 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
 NSAMP = 5000
-FREQ_START = 2
-FREQ_END = 10
+FREQ_START_HZ = 2
+FREQ_END_HZ= 10
+SPIKE_VOLTS = 3.3
 
 t = np.arange(NSAMP)
 
-v = np.sin(2 * np.pi * t * np.linspace(FREQ_START, FREQ_END, NSAMP) / NSAMP)
+f = np.linspace(FREQ_START_HZ, FREQ_END_HZ, NSAMP)
+
+s = np.sin(2 * np.pi * t *  f / NSAMP)
+
+p = signal.find_peaks(s)[0]
+
+v = np.zeros(NSAMP)
+v[p] = 3.3
 
 plt.plot(t, v)
 plt.show()
