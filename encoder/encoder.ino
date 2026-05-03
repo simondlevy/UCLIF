7e6d1561e8f1900b747d728948dd82e6a0550fc4
simondlevy <simon.d.levy@gmail.com>
2026-05-03 17:03:09 -0400

    modified:   encoder.ino

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -4,6 +4,9 @@ static const uint8_t OUTPUT_PIN = 0;
 static const uint32_t SPIKE_DURATION_USEC = 50;
 static const uint32_t SPIKE_INTERVAL_USEC = 200;
 
+static const float FREQ_MIN_HZ = 1000;
+static const float FREQ_MAX_HZ = 10'000;
+
 void setup()
 {
     pinMode(OUTPUT_PIN, OUTPUT);
@@ -11,9 +14,9 @@ void setup()
 
 void loop()
 { 
-    const uint16_t input = 1023 - analogRead(INPUT_PIN);
+    const float freq = FREQ_MIN_HZ + ((1023 - analogRead(INPUT_PIN)) / 1023.f) * (FREQ_MAX_HZ - FREQ_MIN_HZ);
 
-    printf("%d\n", input);
+    printf("%f\n", freq);
 
     digitalWrite(OUTPUT_PIN, HIGH);
     delayMicroseconds(SPIKE_DURATION_USEC);
