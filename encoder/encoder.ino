d486e099fbdf97ef08597e91fd9d5725c20ee716
simondlevy <simon.d.levy@gmail.com>
2026-05-03 16:53:01 -0400

    modified:   encoder/encoder.ino

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,3 +1,4 @@
+static const uint8_t INPUT_PIN = A9;
 static const uint8_t OUTPUT_PIN = 0;
 
 static const uint32_t SPIKE_DURATION_USEC = 50;
@@ -10,6 +11,10 @@ void setup()
 
 void loop()
 { 
+    const uint16_t input = 1023 - analogRead(INPUT_PIN);
+
+    printf("%d\n", input);
+
     digitalWrite(OUTPUT_PIN, HIGH);
     delayMicroseconds(SPIKE_DURATION_USEC);
     digitalWrite(OUTPUT_PIN, LOW);
