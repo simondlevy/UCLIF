de237cb9d964f11805766bdf03266e4840b6d6cb
simondlevy <simon.d.levy@gmail.com>
2026-05-03 20:04:21 -0400

    modified:   encoder.ino

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -2,7 +2,6 @@ static const uint8_t INPUT_PIN = A9;
 static const uint8_t OUTPUT_PIN = 0;
 
 static const uint32_t SPIKE_DURATION_USEC = 50;
-static const uint32_t SPIKE_INTERVAL_USEC = 200;
 
 static const float FREQ_MIN_HZ = 1000;
 static const float FREQ_MAX_HZ = 10'000;
@@ -20,11 +19,9 @@ void loop()
 
     const uint32_t spike_interval_usec = 1'000'000 / freq_hz;
 
-    printf("%d Hz => %lu\n", (int)freq_hz, spike_interval_usec);
-
     digitalWrite(OUTPUT_PIN, HIGH);
     delayMicroseconds(SPIKE_DURATION_USEC);
     digitalWrite(OUTPUT_PIN, LOW);
 
-    delayMicroseconds(SPIKE_INTERVAL_USEC);
+    delayMicroseconds(spike_interval_usec);
 }
