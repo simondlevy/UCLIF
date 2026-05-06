f586c254889c9a97a68d05121e9be9103675aed3
Simon D. Levy <simon.d.levy@gmail.com>
2026-05-05 20:30:35 -0400

    new file:   ltspice/pulse.txt

--- ltspice/mkpwl.py
+++ ltspice/mkpwl.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+'''
+   Python script to generate a .txt file for use as input to the
+   UCLIF neuron
+
+   Copyright (C) 2026 Simon D. Levy
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, in version 3.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http:--www.gnu.org/licenses/>.
+'''
 
 import numpy as np
 import matplotlib.pyplot as plt
